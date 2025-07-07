ada07effc43ed371cbe63169e81123ed46fae2a3
Imil <ibragimovimil0@gmail.com>
2025-07-07 11:24:03 +0300

    [feat] Add packets module

---
+++ internal/api/websocket/packets/guild/packets.go
@@ -0,0 +1,32 @@
+package guild
+
+type Packet interface {
+	isGuildPacket()
+}
+
+type ChatMessage struct {
+	Msg string `json:"content"`
+}
+
+func (ChatMessage) isGuildPacket() {}
+
+type ChatHistory struct {
+	Type string               `json:"type"`
+	Data []ChatHistoryMessage `json:"data"`
+}
+
+func (ChatHistory) isGuildPacket() {}
+
+type ChatHistoryMessage struct {
+	Id        string `json:"_id"`
+	GuildId   int    `json:"guild_id"`
+	UserId    int    `json:"user_id"`
+	Content   string `json:"content"`
+	Timestamp string `json:"timestamp"`
+}
+
+func (ChatHistoryMessage) isGuildPacket() {}
+
+type Disconnect struct{}
+
+func (Disconnect) isGuildPacket() {}
