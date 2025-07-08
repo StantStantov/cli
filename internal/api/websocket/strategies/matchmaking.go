257152dfc7b751bb36f39cfcdd8584b4cb6d19cd
Imil <ibragimovimil0@gmail.com>
2025-07-08 17:30:40 +0300

    [refactor] Check strategies errors

--- internal/api/websocket/strategies/matchmaking.go
+++ internal/api/websocket/strategies/matchmaking.go
@@ -1,8 +1,8 @@
 package strategies
 
 import (
+	"fmt"
 	"lesta-battleship/cli/internal/api/websocket/packets"
-	"log"
 
 	"github.com/gorilla/websocket"
 	matchmaking "github.com/lesta-battleship/matchmaking/pkg/packets"
@@ -15,29 +15,27 @@ import (
 // При получении пакета matchmaking.Disconnect заканчивает работу.
 type MatchmakingStrategy struct{}
 
-func (c *MatchmakingStrategy) ReadPump(readChan chan<- packets.Packet, conn *websocket.Conn) error {
+func (c MatchmakingStrategy) ReadPump(readChan chan<- packets.Packet, conn *websocket.Conn) error {
 	for {
 		packet := matchmaking.Packet{}
 
 		if err := conn.ReadJSON(&packet); err != nil {
-			return err
+			return fmt.Errorf("MatchmakingStrategy.ReadPump: [%w]", err)
 		}
 
 		readChan <- packets.WrapMatchmaking(packet)
 	}
 }
 
-func (c *MatchmakingStrategy) WritePump(writeChan <-chan packets.Packet, conn *websocket.Conn) error {
+func (c MatchmakingStrategy) WritePump(writeChan <-chan packets.Packet, conn *websocket.Conn) error {
 	for packet := range writeChan {
 		unwrap := matchmaking.Packet{}
 		if err := packets.UnwrapAsMatchmaking(packet, &unwrap); err != nil {
-			log.Println(err)
-			return err
+			return fmt.Errorf("MatchmakingStrategy.WritePump: [%w]", err)
 		}
 
 		if err := conn.WriteJSON(unwrap); err != nil {
-			log.Println(err)
-			return err
+			return fmt.Errorf("MatchmakingStrategy.WritePump: [%w]", err)
 		}
 
 		switch unwrap.Body.(type) {
